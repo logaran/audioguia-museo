64dedf15ce0a8a668c3284c1c32b6ed7a6ebbf9d
Antonio Lozano <antonio.lozano@carmenthyssenmalaga.local>
2024-09-05 11:50:47 +0200

    Another try of fixing heigth adjustment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 import AudioGuideApp from './AudioGuideApp';
 
 
 function App() {
   
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);   
+
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" style={{ height:   
+      viewportHeight }}>
       <AudioGuideApp />
     </div>
   );
