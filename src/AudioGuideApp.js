216f803dffbc54093e5662de79a9ac959a9524e4
Antonio Lozano <antonio.lozano@carmenthyssenmalaga.local>
2024-09-11 13:00:41 +0200

    refactor: extracts AudioPlayer component

--- src/AudioGuideApp.js
+++ src/AudioGuideApp.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { ChevronLeft, ChevronRight, Play } from 'lucide-react';
 import { useCookies } from 'react-cookie';
 import IntroScreen from './components/IntroScreen';
 import FavoritesScreen from './components/FavoritesScreen';
@@ -7,6 +6,7 @@ import Header from './components/Header';
 import ControlsBar from './components/ControlsBar';
 import ArtworkInfo from './components/ArtworkInfo';
 import ArtworksList from './components/ArtworksList';
+import AudioPlayer from './components/AudioPlayer';
 
 
 const AudioGuideApp = ({ isMobile }) => {
@@ -15,10 +15,6 @@ const AudioGuideApp = ({ isMobile }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [cookies, setCookie] = useCookies(['likes']);
   const mediaRef = useRef(null);
-  const touchStartX = useRef(0);
-  const touchEndX = useRef(0);
-  const isSwipe = useRef(false);
-  const [swipeOffset, setSwipeOffset] = useState(0);
   const [showIntro, setShowIntro] = useState(true);
   const [showFavorites, setShowFavorites] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState('es');
@@ -55,56 +51,6 @@ const AudioGuideApp = ({ isMobile }) => {
     }
   }, [isPlaying, currentIndex]);
 
-
-  const handleTouchStart = (e) => {
-    touchStartX.current = e.touches[0].clientX;
-    setSwipeOffset(0);
-    isSwipe.current = false;
-  };
-
-  const handleTouchMove = (e) => {
-    touchEndX.current = e.touches[0].clientX;
-    const offset = touchEndX.current - touchStartX.current;
-    setSwipeOffset(offset);
-    if (Math.abs(offset) > 10) {
-      isSwipe.current = true;
-    }
-  };
-
-  const handleTouchEnd = () => {
-    if (isSwipe.current) {
-      if (touchStartX.current - touchEndX.current > 50) {
-        handleSwipe('left');
-      } else if (touchEndX.current - touchStartX.current > 50) {
-        handleSwipe('right');
-      }
-    } else {
-      togglePlayPause();
-    }
-    setSwipeOffset(0);
-  };
-
-  const handleSwipe = (direction) => {
-    if (direction === 'right') {
-      if (currentIndex > 0) {
-        setCurrentIndex(currentIndex - 1);
-      } else if (showIntro) {
-        setShowIntro(false);
-      } else if (showFavorites) {
-        setShowFavorites(false);
-      }
-    } else if (direction === 'left') {
-      if (currentIndex < artworks.length - 1) {
-        setCurrentIndex(currentIndex + 1);
-      } else if (!showFavorites) {
-        setShowFavorites(true);
-      }
-    }
-  };
-
-  const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
 
   const handleLangSelect = (language) => {
     if (isPlaying) setIsPlaying(false);
@@ -156,8 +102,6 @@ const AudioGuideApp = ({ isMobile }) => {
   return (
     <div className="relative h-screen w-full text-black flex flex-col">
 
-
-
       <div
         className="absolute h-100 inset-0 bg-cover bg-center filter blur-md -z-10"
         style={{
@@ -168,12 +112,11 @@ const AudioGuideApp = ({ isMobile }) => {
         }}
       />
 
-
       {/* Cabecera con logo del Museo */}
       <Header />
       {/* Intro */}
       {showIntro && (
-        <IntroScreen toggleShowIntro={toggleShowIntro} langSelect={handleLangSelect} selectedLanguage={selectedLanguage} setShowArtworksList={setShowArtworksList} expositionData={expositionData}/>
+        <IntroScreen toggleShowIntro={toggleShowIntro} langSelect={handleLangSelect} selectedLanguage={selectedLanguage} setShowArtworksList={setShowArtworksList} expositionData={expositionData} />
       )}
       {/* Favoritos (Outro) */}
       {showFavorites && !showIntro && (
@@ -187,69 +130,13 @@ const AudioGuideApp = ({ isMobile }) => {
 
       {/* Player */}
       {!showIntro && !showFavorites &&
-        (
-          <div className="relative h-full flex flex-col justify-between">
-            <div className="relative h-full flex flex-col sm:flex-row sm:justify-evenly items-center justify-evenly w-auto pt-3">
-              <ArtworkInfo artwok={currentArtwork} selectedLanguage={selectedLanguage} />
+        (<>
+          <AudioPlayer currentArtwork={currentArtwork} currentIndex={currentIndex} selectedLanguage={selectedLanguage} isMobile={isMobile} isPlaying={isPlaying} ArtworkInfo={ArtworkInfo} mediaRef={mediaRef} activeResourceUrl={activeResourceUrl} setIsPlaying={setIsPlaying} artworks={artworks} setShowFavorites={setShowFavorites} setShowIntro={setShowIntro} setCurrentIndex={setCurrentIndex} />
 
-              {/* Controles de reproducción y pase de obras*/}
-              <>
-                {/*Play*/}
-                <div
-                  className={`absolute inset-0 flex h-full flex-col justify-between p-4 z-30 transition duration-300 ${isPlaying ? 'pointer-events-auto' : 'bg-black bg-opacity-60 pointer-events-auto'
-                    }`}
-                  onTouchStart={isMobile ? handleTouchStart : null}
-                  onTouchMove={isMobile ? handleTouchMove : null}
-                  onTouchEnd={isMobile ? handleTouchEnd : null}
-                  onClick={!isMobile ? togglePlayPause : null}
-                >
+          {/* Barra de control. Visible en el pase de imagenes */}
+          {!showFavorites && !showIntro && !showArtworksList && <ControlsBar currentArtwork={currentArtwork} toggleLike={toggleLike} langSelect={handleLangSelect} selectedLanguage={selectedLanguage} cookies={cookies} handleShowArtworks={handleShowArtworksList} />}
 
-                  {!isPlaying && (
-                    <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-400 text-3xl z-30">
-                      <Play color="white" size={64} />
-                    </div>
-                  )}
-                </div>
-
-                {/*Flechas*/}
-                {currentIndex !== 0 && (
-                  <button
-                    className="absolute left-0 top-1/2 transform -translate-y-1/2 text-gray-400 text-3xl z-30"
-                    onClick={() => handleSwipe('right')}
-                  >
-                    <ChevronLeft size={48} />
-                  </button>
-                )}
-                <button
-                  className="absolute right-0 top-1/2 transform -translate-y-1/2 text-gray-400 text-3xl z-30"
-                  onClick={() => handleSwipe('left')}
-                >
-                  <ChevronRight size={48} />
-                </button>
-              </>
-
-              <img
-                src={currentArtwork.imageUrl[selectedLanguage]}
-                alt={currentArtwork.name}
-                className="transition-transform h-[50vh] duration-300 object-contain"
-                style={{ transform: `translateX(${swipeOffset}px)` }}
-              />
-
-
-            </div>
-
-            <audio
-              ref={mediaRef}
-              src={activeResourceUrl}
-              onEnded={() => setIsPlaying(false)}
-            />
-
-            {/* Barra de control. Visible en el pase de imagenes */}
-            {!showFavorites && !showIntro && !showArtworksList && <ControlsBar currentArtwork={currentArtwork} toggleLike={toggleLike} langSelect={handleLangSelect} selectedLanguage={selectedLanguage} cookies={cookies} handleShowArtworks={handleShowArtworksList} />}
-
-
-
-          </div>
+        </>
         )}
       {/* Advertencia en modo escritorio 
       {!isMobile && (
